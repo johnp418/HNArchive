59a1dbce13b9b29c399ed213a4092f256ab46f7a
JohnPark <jongmanpark418@gmail.com>
2017-08-07 18:47:56 -0700

    Bugfix in retrieving date string

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-if (process.argv.length < 2) {
-  module.exports = (options) => {
-    return require('./lib/index')(options);
-  }
-} else {
-  const exported = require('./lib/index')();
-  exported.scrape();
+/* if (process.argv.length < 2) {
+ *   module.exports = (options) => {
+ *     return require('./lib/index')(options);
+ *   }
+ * } else {
+ *   const exported = require('./lib/index')();
+ *   exported.scrape();
+ * }
+ * 
+ * */
+
+module.exports = (opt) => {
+  console.log(process.argv);
+  return require('./lib/index')(opt);
 }
 
+/* const HN = require('./lib/index')();*/
+/* HN.scrape();*/
+/* let date = '2017-08-06';
+ * 
+ * HN.getJSON(date).then((data) => {
+ *   console.log(data)
+ * }).catch(err => {
+ *   console.log('err', err);
+ * })*/
